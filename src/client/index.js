35fd36d9476d1843ddd2a979ebe1f6ab183def05
kalidasrajendran <kalidasrajendran1992@gmail.com>
2020-01-15 21:45:20 +0100

    feat: dev server working

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 function App(){
-  return <h1>Hello World;</h1>;
+  return <h1>Hello World; dev server test</h1>;
 }
 
 render(<App />, document.getElementById('root'));
